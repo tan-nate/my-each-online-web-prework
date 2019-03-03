aede6f3f3f0cccbaf5828ce48f2a7535525ab0bc
Nate Tan <nktan93@gmail.com>
2019-03-03 18:28:19 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,18 +1,14 @@
 array = [1, 2, 3, 4, 5]
 
 def my_each(array)
-  if block_given?
-    i = 0
-  
-    while i < array.length
-      yield array[i]
-    i = i + 1
-    end
-  
-    array
-  else
-    puts "Hey! No block was given!"
+  i = 0
+
+  while i < array.length
+    yield array[i]
+  i = i + 1
   end
+
+  array
 end
 
 my_each(array) do |item|
